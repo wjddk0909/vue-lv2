0ac3e1744bc464b7d523e9080a6c687a428851f8
ellin-hand <ellin.hand@kakaocorp.com>
2022-04-20 08:17:53 +0900

    TodoInput 컴포넌트 할 일 저장 기능 구혀

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <input type="text" v-model="newTodoItem">
+        <button v-on:click="addTodo">add</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            newTodoItem: ""
+        }
+    },
+    methods: {
+        addTodo: function() {
+            console.log(this.newTodoItem);
+            this.newTodoItem = '';
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
