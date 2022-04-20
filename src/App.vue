bed7e97fb8c99b864ad5a8fe2ea8e5b99f53f81e
ellin-hand <ellin.hand@kakaocorp.com>
2022-04-20 21:13:30 +0900

    [리팩토링] 할 일 모두 삭제 기능

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <TodoHeader></TodoHeader>
     <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
     <TodoList v-bind:propsdata="todoItems" v-on:removeItem="removeOneItem" v-on:toggleItem="toggleItem"></TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoFooter v-on:clearAll="clearAllItems"></TodoFooter>
   </div>
 </template>
 
@@ -38,6 +38,10 @@ export default {
       this.todoItems[index].completed = !this.todoItems[index].completed;
       localStorage.removeItem(todoItem.item);
       localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+    clearAllItems: function() {
+      localStorage.clear();
+      this.todoItems = []
     }
   },
   created: function() {
