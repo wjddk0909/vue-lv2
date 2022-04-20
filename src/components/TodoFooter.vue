9a6666c8c627624742ceec8d5ffdd80cbf96e1d0
ellin-hand <ellin.hand@kakaocorp.com>
2022-04-20 14:50:43 +0900

    현재 앱 구조의 문제점 진단 및 개선된 앱 구조 소개

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -8,7 +8,10 @@
 export default {
     methods: {
         clearTodo: function() {
-            localStorage.clear(); // localStorage 지우는 api
+            localStorage.clear(); 
+            // localStorage 지우는 api 
+            // 이렇게 작성하면(삭제 뿐만 아니라 todoInput쪽 할일 추가 부분도) localStorage에서는 지워지지만(추가되지만 브라우저는 새로고침해야 반영) TodoList컴포넌트와 분리되어 있기 때문에 list에는 영향이 없다. 
+            // 그래서 하위 컴포넌트(todoFooter, todoInput)에서는 event로 상위(App)에 보내고 상위에서는 해당 내용을 props로 내려줍니다.
         }
     }
 };
