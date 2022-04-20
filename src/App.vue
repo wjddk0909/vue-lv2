e1d3fc5b6021b75ce65ec88033fcef38f7d80a7d
ellin-hand <ellin.hand@kakaocorp.com>
2022-04-20 15:02:13 +0900

    [리팩토링] 할 일 목록 표시 기능

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <TodoHeader></TodoHeader>
     <TodoInput></TodoInput>
-    <TodoList></TodoList>
+    <!-- <TodoList v-bind:내려보낼 프롭스 속성 이름="현재위치의 컴포넌트 데이터 속성"></TodoList> -->
+    <TodoList v-bind:propsdata="todoItems"></TodoList>
     <TodoFooter></TodoFooter>
   </div>
 </template>
@@ -14,6 +15,20 @@ import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
 export default {
+  // 각각의 컴포넌트에서 사용하던 데이터 로직들을 다 App.vue로 끌어옴
+  // todoInput에 있던 내용
+  data: function() {
+    return {
+      todoItems: []
+    }
+  },
+  created: function() {
+    if(localStorage.length > 0) {
+        for (var i = 0; i < localStorage.length; i++) {
+            this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
+    }
+  },
   components: {
     'TodoHeader': TodoHeader,
     'TodoInput': TodoInput,
